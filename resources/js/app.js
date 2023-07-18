944ae1e7cb509979814cf92974774d214f0430e6
unknown <webtg20@gmail.com>
2023-07-18 11:45:28 +0530

    laravel vue js file

---
+++ resources/js/app.js
@@ -0,0 +1,8 @@
+import './bootstrap';
+
+import {createApp} from 'vue'; //imoprt this form vue 
+
+import app from './component/app.vue';  //imoprt this form component folder with path
+
+createApp(app).mount('#app') // call which file use s main file 
+
---
+++ resources/views/doc.php
@@ -0,0 +1,16 @@
+laravel new Vite_laravel
+
+npm install vue@next <!--It builds on top of standard HTML, CSS, and JavaScript and provides a declarative and component-based programming model that helps you efficiently develop user interfaces, be they simple or complex.-->
+
+vue-loader <!--- --->
+
+npm install <!---- With npm, developers can easily manage dependencies for their projects.-->
+
+npm i @vitejs / plugin-vue <!--vite use this to run vue help us this plugin-->
+
+npm run dev <!--would start the development server using-->
+
+webpack.mix.js remove -- vita.config.js
+
+<!---install -def-->
+<!-- ------------------------ When running vite from the command line, Vite will automatically try to resolve a config file named vite.config.js inside project root (other JS and TS extensions are also supported). -->
